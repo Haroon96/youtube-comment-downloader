8f434063325bd5e6149b276624c20c1e89af428a
Muhammad Haroon <m_haroon96@hotmail.com>
2021-07-06 13:46:52 -0700

    added programmatic API

--- youtube_comment_downloader/downloader.py
+++ youtube_comment_downloader/downloader.py
@@ -2,11 +2,7 @@
 
 from __future__ import print_function
 
-import argparse
-import io
 import json
-import os
-import sys
 import time
 
 import requests
@@ -103,14 +99,16 @@ def download_comments(youtube_id, sort_by=SORT_BY_RECENT, sleep=.1):
                                   for ncd in search_dict(response, 'nextContinuationData')])
 
         for comment in search_dict(response, 'commentRenderer'):
-            yield {'cid': comment['commentId'],
-                   'text': ''.join([c['text'] for c in comment['contentText'].get('runs', [])]),
-                   'time': comment['publishedTimeText']['runs'][0]['text'],
-                   'author': comment.get('authorText', {}).get('simpleText', ''),
-                   'channel': comment['authorEndpoint']['browseEndpoint'].get('browseId', ''),
-                   'votes': comment.get('voteCount', {}).get('simpleText', '0'),
-                   'photo': comment['authorThumbnail']['thumbnails'][-1]['url'],
-                   'heart': next(search_dict(comment, 'isHearted'), False)}
+            yield dict(
+                video_id=youtube_id,
+                comment_id=comment['commentId'],
+                author_id=comment.get('authorEndpoint', {}).get('browseEndpoint', {}).get('browseId'),
+                author_name=comment.get('authorText', {}).get('simpleText', ''),
+                comment=''.join([c['text'] for c in comment['contentText'].get('runs', [])]),
+                likes=comment.get('voteCount', {}).get('simpleText', '0'),
+                published_at=comment['publishedTimeText']['runs'][0]['text'],
+                parent_id=None
+            )
 
         time.sleep(sleep)
 
@@ -128,53 +126,3 @@ def search_dict(partial, search_key):
         elif isinstance(current_item, list):
             for value in current_item:
                 stack.append(value)
-
-
-def main(argv = None):
-    parser = argparse.ArgumentParser(add_help=False, description=('Download Youtube comments without using the Youtube API'))
-    parser.add_argument('--help', '-h', action='help', default=argparse.SUPPRESS, help='Show this help message and exit')
-    parser.add_argument('--youtubeid', '-y', help='ID of Youtube video for which to download the comments')
-    parser.add_argument('--output', '-o', help='Output filename (output format is line delimited JSON)')
-    parser.add_argument('--limit', '-l', type=int, help='Limit the number of comments')
-    parser.add_argument('--sort', '-s', type=int, default=SORT_BY_RECENT,
-                        help='Whether to download popular (0) or recent comments (1). Defaults to 1')
-
-    try:
-        args = parser.parse_args() if argv is None else parser.parse_args(argv)
-
-        youtube_id = args.youtubeid
-        output = args.output
-        limit = args.limit
-
-        if not youtube_id or not output:
-            parser.print_usage()
-            raise ValueError('you need to specify a Youtube ID and an output filename')
-
-        if os.sep in output:
-            outdir = os.path.dirname(output)
-            if not os.path.exists(outdir):
-                os.makedirs(outdir)
-
-        print('Downloading Youtube comments for video:', youtube_id)
-        count = 0
-        with io.open(output, 'w', encoding='utf8') as fp:
-            sys.stdout.write('Downloaded %d comment(s)\r' % count)
-            sys.stdout.flush()
-            start_time = time.time()
-            for comment in download_comments(youtube_id, args.sort):
-                comment_json = json.dumps(comment, ensure_ascii=False)
-                print(comment_json.decode('utf-8') if isinstance(comment_json, bytes) else comment_json, file=fp)
-                count += 1
-                sys.stdout.write('Downloaded %d comment(s)\r' % count)
-                sys.stdout.flush()
-                if limit and count >= limit:
-                    break
-        print('\n[{:.2f} seconds] Done!'.format(time.time() - start_time))
-
-    except Exception as e:
-        print('Error:', str(e))
-        sys.exit(1)
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
